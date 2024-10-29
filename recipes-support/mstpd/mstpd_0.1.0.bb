bisdn/meta-bisdn-linux

commit a9d0100212fe6b6c4d9f6d7f4e913346476848c2

mstpd: fix path cost calculation for link speeds > 65G
Update mstpd to fix path cost calculation for link speeds > 65G.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-support/mstpd/mstpd_0.1.0.bb
@@ -7,8 +7,8 @@ SRC_URI = "\
   git://github.com/bisdn/mstpd.git;branch=${PV};protocol=https \
   file://bridge-stp.conf"
 
-PV:append = "+bisdn5"
-SRCREV = "ab12e96c5e459f976f7c86051792af735e4dde0f"
+PV:append = "+bisdn6"
+SRCREV = "809b744636307dce72807ec5e73e1a10f4ccd008"
 
 S = "${WORKDIR}/git"
 

commit 143482451bfd82c63bf8b6e1173ef9fc95b24170

mstpd: correctly reported version as 0.1.0+bisdn6
The 0.1.0+bisdn6 branch missed the version bump, so the binary still
reported as 0.1.0+bisdn5. Fix this by moving to the correct commit.

Fixes: a9d0100212fe ("mstpd: fix path cost calculation for link speeds > 65G")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-support/mstpd/mstpd_0.1.0.bb
@@ -8,7 +8,7 @@ SRC_URI = "\
   file://bridge-stp.conf"
 
 PV:append = "+bisdn6"
-SRCREV = "809b744636307dce72807ec5e73e1a10f4ccd008"
+SRCREV = "e3424dff8667bd901a9a349ab18718159b65b263"
 
 S = "${WORKDIR}/git"
 